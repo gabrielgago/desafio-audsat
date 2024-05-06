3bf731cfa8e85b59331a7e49da656f0a99500fec
gabrielgago <gfrael@gmail.com>
2024-05-06 19:22:58 -0300

    Created project

---
+++ src/main/java/com/desafio/audsat/AudsatApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.audsat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudsatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudsatApplication.class, args);
+    }
+
+}
