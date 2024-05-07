e4c794269bf18c8c44ef31cfe7fe5347e478079c
gabrielgago <gfrael@gmail.com>
2024-05-06 21:31:06 -0300

    create model Customer.

---
+++ src/main/java/com/desafio/audsat/domain/Customer.java
@@ -0,0 +1,27 @@
+package com.desafio.audsat.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "driver_id", referencedColumnName = "id")
+    private Driver driver;
+
+    @OneToMany(mappedBy = "customer")
+    private Set<Insurance> insurances = new HashSet<>();
+}
