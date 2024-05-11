60039330d3902e90fc056dbb8d5076c26ccb5902
gabrielgago <gfrael@gmail.com>
2024-05-11 18:02:10 -0300

    Added forgotten annotation.

--- src/main/java/com/desafio/audsat/repository/CustomerRepository.java
+++ src/main/java/com/desafio/audsat/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.desafio.audsat.repository;
 
 import com.desafio.audsat.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
