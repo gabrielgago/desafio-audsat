fac66e3e4f91507c46487f49c15b9a1a66033462
gabrielgago <gfrael@gmail.com>
2024-05-11 18:05:30 -0300

    Refactor customerDTO.

--- src/main/java/com/desafio/audsat/dto/CustomerDTO.java
+++ src/main/java/com/desafio/audsat/dto/CustomerDTO.java
@@ -2,19 +2,18 @@ package com.desafio.audsat.dto;
 
 import com.desafio.audsat.domain.Customer;
 import com.desafio.audsat.domain.Driver;
-import com.desafio.audsat.request.CustomerRequest;
-import com.desafio.audsat.response.InsuranceResponse;
+import com.desafio.audsat.domain.Insurance;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.br.CPF;
 
-import java.util.List;
+import java.util.Set;
 
 public record CustomerDTO(@NotNull String name, @NotNull @Email String email, @NotNull @CPF String document,
-                          Driver driver, List<InsuranceResponse> insurances) {
+                          Driver driver, Set<Insurance> insurances) {
 
-    public static CustomerRequest of(Customer customer) {
-        return new CustomerRequest(customer.getName(), customer.getEmail(), customer.getDocument());
+    public static CustomerDTO of(Customer customer) {
+        return new CustomerDTO(customer.getName(), customer.getEmail(), customer.getDocument(), customer.getDriver(), customer.getInsurances());
     }
 
     public Customer toCustomer() {
